7f42e177a1d36ef70747bb776229961c3e4ebf19
Adam Langley <agl@chromium.org>
2010-05-25 15:13:53 -0400

    Initial commit

---
+++ shamirsplit.go
@@ -0,0 +1,138 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The shamirsplit package implements Shamir's cryptographic secret sharing
+// algorithm.
+package shamirsplit
+
+import (
+	"big"
+	"io"
+	"os"
+)
+
+// Split takes a secret number and returns n shares where any k shares can be
+// combined to recover the original secret. However, possession of less than k
+// shares reveals nothing about the secret.
+func Split(secret *big.Int, modulus *big.Int, k, n int, rand io.Reader) (shares []*big.Int, err os.Error) {
+	if k < 1 || n < k {
+		return nil, os.ErrorString("invalid split parameters")
+	}
+
+	if secret.Cmp(modulus) >= 0 {
+		return nil, os.ErrorString("secret must be less than split modulus")
+	}
+
+	a := make([]*big.Int, k)
+	a[0] = secret
+	one := big.NewInt(1)
+	modulusMinus1 := new(big.Int)
+	modulusMinus1.Sub(modulus, one)
+
+	for i := 1; i < k; i++ {
+		a[i], err = randomNumber(rand, modulusMinus1)
+		if err != nil {
+			return
+		}
+		a[i].Add(a[i], one)
+	}
+
+	shares = make([]*big.Int, n)
+
+	for i := 1; i <= n; i++ {
+		bigI := big.NewInt(int64(i))
+		t := new(big.Int)
+
+		for j := 0; j < k; j++ {
+			e := new(big.Int).Exp(bigI, big.NewInt(int64(j)), nil)
+			e.Mul(e, a[j])
+			t.Add(t, e)
+		}
+
+		t.Mod(t, modulus)
+		shares[i-1] = t
+	}
+
+	return
+}
+
+// Join takes k shares that resulted from Split and recovers the original
+// secret. The shares can be presented in any order, however the (zero based)
+// index of each share must be known and provided in shareNumbers.
+func Join(shares []*big.Int, shareNumbers []int, modulus *big.Int) (*big.Int, os.Error) {
+	if len(shares) != len(shareNumbers) {
+		return nil, os.ErrorString("lengths of shares and shareNumbers must match")
+	}
+
+	secret := new(big.Int)
+	zero := new(big.Int)
+
+	for i := 0; i < len(shares); i++ {
+		if shareNumbers[i] < 0 {
+			return nil, os.ErrorString("found negative share number")
+		}
+
+		c := big.NewInt(1)
+		for j := 0; j < len(shares); j++ {
+			if i == j {
+				continue
+			}
+			bigJ := big.NewInt(int64(shareNumbers[j] + 1))
+			c.Mul(c, bigJ)
+			bigJ.Sub(bigJ, big.NewInt(int64(shareNumbers[i]+1)))
+			if bigJ.Cmp(zero) < 0 {
+				bigJ.Add(bigJ, modulus)
+			}
+
+			d := new(big.Int)
+			x := new(big.Int)
+			y := new(big.Int)
+			big.GcdInt(d, x, y, bigJ, modulus)
+			if x.Cmp(zero) < 0 {
+				x.Add(x, modulus)
+			}
+			c.Mul(c, x)
+			c.Mod(c, modulus)
+		}
+
+		c.Mul(c, shares[i])
+		secret.Add(secret, c)
+	}
+
+	secret.Mod(secret, modulus)
+	return secret, nil
+}
+
+// randomNumber returns a uniform random value in [0, max).
+func randomNumber(rand io.Reader, max *big.Int) (n *big.Int, err os.Error) {
+	k := (max.BitLen() + 7) / 8
+
+	// r is the number of bits in the used in the most significant byte of
+	// max.
+	r := uint(max.BitLen() % 8)
+	if r == 0 {
+		r = 8
+	}
+
+	bytes := make([]byte, k)
+	n = new(big.Int)
+
+	for {
+		_, err = io.ReadFull(rand, bytes)
+		if err != nil {
+			return
+		}
+
+		// Clear bits in the first byte to increase the probability
+		// that the candidate is < max.
+		bytes[0] &= uint8(int(1<<r) - 1)
+
+		n.SetBytes(bytes)
+		if n.Cmp(max) < 0 {
+			return
+		}
+	}
+
+	return
+}
---
+++ shamirsplit_test.go
@@ -0,0 +1,53 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package shamirsplit
+
+import (
+	"big"
+	"os"
+	"testing"
+)
+
+// 2^2048 - 2^1984 - 1 + 2^64 * { [2^1918 pi] + 124476 } from RFC3526
+const modulusStr = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF"
+
+func TestSplitting(t *testing.T) {
+	urandom, err := os.Open("/dev/urandom", os.O_RDONLY, 0)
+	if err != nil {
+		t.Errorf("failed to open /dev/urandom")
+	}
+
+	const k = 10
+	const n = 100
+
+	secret := big.NewInt(42)
+	modulus, _ := new(big.Int).SetString(modulusStr, 16)
+	shares, err := Split(secret, modulus, k, n, urandom)
+	if err != nil {
+		t.Errorf("error while splitting: %s", err)
+		return
+	}
+
+	if len(shares) != n {
+		t.Errorf("too few shares returned")
+		return
+	}
+
+	shareNumbers := make([]int, n)
+	for i := 0; i < n; i++ {
+		shareNumbers[i] = i
+	}
+
+	for i := 0; i < n-k; i++ {
+		result, err := Join(shares[i:k+i], shareNumbers[i:k+i], modulus)
+		if err != nil {
+			t.Errorf("failed to join shares: %s", err)
+		}
+
+		if result.Cmp(secret) != 0 {
+			t.Errorf("Join returned wrong value with %d shares (want: %s, got: %s)", i, secret, result)
+		}
+	}
+}
